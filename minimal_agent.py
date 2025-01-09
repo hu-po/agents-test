671f7a3bc557b311b22d3efd950c2866c9e112d8
hu.po <hugo.p.cmu@gmail.com>
2025-01-09 10:20:39 -0600

    asd

--- minimal_agent.py
+++ minimal_agent.py
@@ -1,8 +1,9 @@
 goal = ""
 memory = []
+env = Environment()
 while True:
-    observation = sense()
+    observation = sense(env)
     action = plan(goal, observation, memory)
-    outcome = act(action)
+    outcome = act(env, action)
     memory.append(observation, outcome)
     goal = update_goal(goal, outcome)
